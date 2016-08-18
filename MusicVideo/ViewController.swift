8697c5f4cbd32cdbde387905840e93d8c60c196c
mojodnastream <gnothom@gmail.com>
2016-08-18 14:20:41 -0700

    Added reachability observer to view controller...

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     
     var videos = [Videos]()
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
         
         //call API
         let api = APIManager()
@@ -30,6 +36,24 @@ class ViewController: UIViewController {
             print("\(index) name = \(item.vName)")
         }
         
+    }
+    
+    func reachabilityStatusChanged() {
+        switch reachabilityStatus {
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+            displayLabel.text = "No Internet For You"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+            displayLabel.text = "You are connected to WiFIs - Yay"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+            displayLabel.text = "Be nice, you are on the networks"
+        default:return
+        }
+    }
+    
+    //blow out the observers
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:"ReachStatusChanged", object: nil)
     }
     
 }
