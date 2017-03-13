0f8a3b0f11f01dd68117a10ad890ac3195ac6e3e
mojodnastream <gnothom@gmail.com>
2017-03-13 15:49:35 -0700

    Implemented switch logic for touchid

--- MusicVideo/SettingsTVC.swift
+++ MusicVideo/SettingsTVC.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class SettingsTVC: UITableViewController {
     
+    @IBAction func touchIdSec(_ sender: UISwitch) {
+        let defaults = UserDefaults.standard
+        
+        if touchID.isOn {
+            defaults.set(touchID.isOn, forKey: "SecSetting")
+        }
+        else {
+            defaults.set(false, forKey: "SecSetting")
+        }
+        
+    }
     @IBOutlet weak var aboutDisplay: UILabel!
     @IBOutlet weak var feedbackDisplay: UILabel!
     @IBOutlet weak var securityDisplay: UILabel!
@@ -24,14 +35,21 @@ class SettingsTVC: UITableViewController {
         tableView.alwaysBounceVertical = false
         
         NotificationCenter.default.addObserver(self, selector: #selector(SettingsTVC.preferredFontChanged), name: NSNotification.Name.UIContentSizeCategoryDidChange, object: nil)
+        
+        title = "settings"
 
-
+        
+        touchID.isOn = UserDefaults.standard.bool(forKey: "SecSetting")
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+   
+
     
     func preferredFontChanged() {
         aboutDisplay.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
