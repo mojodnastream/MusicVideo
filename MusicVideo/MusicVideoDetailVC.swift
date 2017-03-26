53c49c36ecc4100357a41e9201c7c7241fab78bf
mojodnastream <gnothom@gmail.com>
2017-03-26 10:30:00 -0700

    Added social media sharing

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -14,6 +14,54 @@ class MusicVideoDetailVC: UIViewController {
     
     var videos:Videos!
     
+    @IBAction func socialMedia(_ sender: UIBarButtonItem) {
+        shareMedia()
+    }
+    
+    func shareMedia() {
+        let activity1 = "Have you had the opportunity to see this Music Video?"
+        let activity2 = ("\(videos.vName) by \(videos.vArtist)")
+        let activity3 = "Watch it and tell me what you think?"
+        let activity4 = videos.vLinkToiTunes
+        let activity5 = "(Shared with the Music Video App - Step It UP!)"
+        
+        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: [activity1, activity2, activity3, activity4,activity5], applicationActivities: nil)
+        
+        //activityViewController.excludedActivityTypes =  [UIActivityTypeMail]
+        
+        
+        
+        //        activityViewController.excludedActivityTypes =  [
+        //            UIActivityTypePostToTwitter,
+        //            UIActivityTypePostToFacebook,
+        //            UIActivityTypePostToWeibo,
+        //            UIActivityTypeMessage,
+        //            UIActivityTypeMail,
+        //            UIActivityTypePrint,
+        //            UIActivityTypeCopyToPasteboard,
+        //            UIActivityTypeAssignToContact,
+        //            UIActivityTypeSaveToCameraRoll,
+        //            UIActivityTypeAddToReadingList,
+        //            UIActivityTypePostToFlickr,
+        //            UIActivityTypePostToVimeo,
+        //            UIActivityTypePostToTencentWeibo
+        //        ]
+        
+        activityViewController.completionWithItemsHandler = {
+            (activity, success, items, error) in
+            
+            if activity == UIActivityType.mail {
+                print ("email selected")
+            }
+            
+        }
+        
+        self.present(activityViewController, animated: true, completion: nil)
+        
+        
+    }
+    
+    
     @IBAction func playVideo(_ sender: UIBarButtonItem) {
         
         let url = NSURL(string: videos.vVideoUrl)!
@@ -32,6 +80,7 @@ class MusicVideoDetailVC: UIViewController {
     @IBOutlet weak var vPrice: UILabel!
     @IBOutlet weak var vRights: UILabel!
     
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         
